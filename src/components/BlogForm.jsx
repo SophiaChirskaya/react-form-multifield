1525a4dea5e9a6be694354ed607fd5774e17041b
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-19 18:16:25 +0100

    [edit]form's input

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -38,12 +38,58 @@ const blogPosts = [
     } 
     ];
 
+    const initialFormData = {
+        title: "",
+        author: "",
+        content: "",
+        category: "",
+    };
+
     const BlogForm = () => {
         const [posts, setPosts] = useState(blogPosts);
+        const [formData, setFormData] = useState(initialFormData);
 
         return (
             <>
             <h2>Dati del blog</h2>
+            <form action="#">
+                <input
+                type="text"
+                name="title"
+                onChange={handleFormData}
+                value={formData.title}
+                placeholder="Titolo blog"
+                 />
+
+                <input
+                type="text"
+                name="author"
+                onChange={handleFormData}
+                value={formData.author}
+                placeholder="Autore del blog"
+                 />
+
+                <input
+                type="text"
+                name="content"
+                onChange={handleFormData}
+                value={formData.content}
+                placeholder="Contenuto del blog"
+                 />
+
+                <input
+                type="text"
+                name="category"
+                onChange={handleFormData}
+                value={formData.category}
+                placeholder="Categoria"
+                 />
+            </form>
+
+
+
+
+
             {
                 posts.map((post, I) => (
                     <div className="postItem" key={post.id}>
